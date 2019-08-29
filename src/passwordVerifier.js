5403b8ff87684312cef7b81c385339bec1da2585
royo <roy@osherove.com>
2019-08-29 21:42:00 +0300

    initial

--- src/passwordVerifier.js
+++ src/passwordVerifier.js
@@ -1,25 +1,26 @@
-const makeVerifier = (rules) => {
-  const _rules = [];
-  const verifier = {
-    addRule: function (rule) {
-      _rules.push(rule);
-    },
-    verify: function (input) {
-      for (let i = 0; i < _rules.length; i++) {
-        const rule = _rules[i];
-        const resultObj = rule(input);
-        if (!resultObj.result) {
-          return resultObj;
-        }
+const _rules = [];
+const verifier = {
+  addRule: rule => {
+    _rules.push(rule);
+  },
+  verify: input => {
+    for (let i = 0; i < _rules.length; i++) {
+      const rule = _rules[i];
+      const resultObj = rule(input);
+      if (!resultObj.result) {
+        return resultObj;
       }
-      return {
-        result: true,
-        reason: ''
-      };
     }
-  };
+    return {
+      result: true,
+      reason: ''
+    };
+  }
+};
 
+const makeVerifier = (rules) => {
   return verifier;
 };
+
 
 exports.makeVerifier = makeVerifier;
