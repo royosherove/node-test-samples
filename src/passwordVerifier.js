d327219d5ab82e93e150dd6cd29ee1c6fd312a6b
royo <roy@osherove.com>
2019-08-31 20:21:24 +0300

    cuntional - ctor verion

--- src/passwordVerifier.js
+++ src/passwordVerifier.js
@@ -1,25 +1,25 @@
-const _rules = [];
-const verifier = {
-  addRule: rule => {
-    _rules.push(rule);
-  },
-  verify: input => {
-    for (let i = 0; i < _rules.length; i++) {
-      const rule = _rules[i];
-      const resultObj = rule(input);
-      if (!resultObj.result) {
-        return resultObj;
+const Verifier = () => {
+  const __rules = [];
+  return {
+    addRule: rule =>  __rules.push(rule) ,
+    verify: input => {
+      for (let i = 0; i < __rules.length; i++) {
+        const rule = __rules[i];
+        const resultObj = rule(input);
+        if (resultObj.result === false) {
+          return resultObj;
+        }
       }
-    }
-    return {
-      result: true,
-      reason: ''
-    };
-  }
+      return {
+        result: true,
+        reason: ''
+      };
+    },
+  };
 };
 
-const makeVerifier = (rules) => {
-  return verifier;
+const makeVerifier = () => {
+  return Object.assign({}, Verifier());
 };
 
 exports.makeVerifier = makeVerifier;
