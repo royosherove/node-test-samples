9e1ce5f10d4f53afb70b5626138f03a3c3851bf9
royo <roy@osherove.com>
2019-09-01 00:35:41 +0300

    bit of refactoring

--- src/passwordVerifier-factory.js
+++ src/passwordVerifier-factory.js
@@ -1,26 +1,29 @@
+const verify = (rules,input) => {
+    const payload = {errors: [], reasons: '', input};
+    const reduced = rules.reduce(reducer, payload);
+    return {
+        result: reduced.errors.length === 0,
+        reason: reduced.reasons
+    };
+};
+
+const reducer = ({errors, reasons, input}, rule) => {
+    const result = rule(input);
+    if (result.result === false) {
+        return {errors:[...errors, result], reasons:reasons + result.reason}
+    }
+    return {errors, reasons, input}
+};
 const Verifier = () => {
-  const __rules = [];
-  return {
-    addRule: rule =>  __rules.push(rule) ,
-    verify: input => {
-      const reducer = ({errors, reasons}, rule) => {
-        const result = rule(input);
-        if (result.result === false) {
-          return {errors:[...errors, result], reasons:reasons + result.reason}
-        }
-          return {errors, reasons}
-      };
-        const payload = {errors:[], reasons: ''};
-        const reduced =  __rules.reduce(reducer, payload);
-        return { result: reduced.errors.length===0,
-            reason: reduced.reasons
-        };
-    },
-  };
+    const __rules = [];
+    return {
+        addRule: rule =>  __rules.push(rule) ,
+        verify: input => verify(__rules, input),
+    };
 };
 
 const makeVerifier = () => {
-  return Object.assign({}, Verifier());
+    return Object.assign({}, Verifier());
 };
 
 exports.makeVerifier = makeVerifier;
